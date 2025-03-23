466a404b8857314dd7b92c6b5d7dce89390dec6f
Eduardo Takashi Missaka <10417877@mackenzista.com.br>
2025-03-22 22:26:57 -0300

    Project barber-shop-ui

---
+++ src/app/clients/edit-client/edit-client.component.ts
@@ -0,0 +1,62 @@
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { SERVICES_TOKEN } from '../../services/service.token';
+import { ClientsService } from '../../services/api-client/clients/clients.service';
+import { IClientService } from '../../services/api-client/clients/iclients.service';
+import { ClientFormComponent } from "../components/client-form/client-form.component";
+import { SnackbarManagerService } from '../../services/snackbar-manager.service';
+import { ISnackbarManagerService } from '../../services/isnackbar-manager.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClientModelForm } from '../clients.models';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-edit-client',
+  imports: [ClientFormComponent],
+  templateUrl: './edit-client.component.html',
+  styleUrl: './edit-client.component.scss',
+    providers: [
+        { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService },
+        { provide : SERVICES_TOKEN.SNACKBAR, useClass: SnackbarManagerService}
+      ]
+})
+export class EditClientComponent implements OnInit, OnDestroy{
+
+  private httpsubscription: Subscription[] = []
+  client: ClientModelForm = { id: 0, name: '', email: '', phone: ''}
+
+  constructor(
+    @Inject(SERVICES_TOKEN.HTTP.CLIENT)private readonly httpService : IClientService,
+    @Inject(SERVICES_TOKEN.SNACKBAR) private readonly snackBarManager: ISnackbarManagerService,
+    private readonly activatedRoute : ActivatedRoute,
+    private readonly router : Router
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id')
+    if(!id){
+      this.snackBarManager.show('Informações não encontradas')
+      this.router.navigate(['clients/list'])
+      return
+    }
+    this.httpsubscription?.push(this.httpService.findById(Number(id)).subscribe(data => this.client = data))
+  }
+
+  ngOnDestroy(): void {
+    this.httpsubscription.forEach(s => s.unsubscribe())
+  }
+
+
+  onSubmitClient(value: ClientModelForm){
+    const { id, ...request} = value
+    if (id){
+      this.httpsubscription?.push(this.httpService.update(id, request).subscribe(_ => {
+        this.snackBarManager.show('Usuário foi atualizado'),
+        this.router.navigate(['clients/list'])
+
+      }))
+      return
+    }
+    this.snackBarManager.show('Erro')
+    this.router.navigate(['clients/list'])
+  }
+}
---
+++ src/app/services/api-client/schedules/ischedules.service.ts
@@ -0,0 +1,11 @@
+import { Observable } from "rxjs";
+import { SaveScheduleRequest, SaveScheduleResponse, ScheduleAppointmentMonthResponse } from "./schedules.models";
+
+export interface IScheduleService{
+
+  save(request: SaveScheduleRequest): Observable<SaveScheduleResponse>
+
+  delete(id: number) : Observable<void>
+
+  listInMonth(year: number, month: number): Observable<ScheduleAppointmentMonthResponse>
+}
